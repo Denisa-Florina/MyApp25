242e36f212e47246c1c33349e151d0acaf5d775f
Ioan Lazar <ilazar@Ioans-MacBook-Pro.local>
2025-10-30 16:54:54 +0200

    04

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapp.todo.data.Item
+import com.example.myapp.todo.ui.ItemDetail
 
 val TAG = "MainActivity"
 
@@ -14,8 +16,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Log.d(TAG, "setContent")
-            ItemDetail("Learn compose")
+            Log.d(TAG, "onCreate")
+            ItemDetail(Item(text = "Learn compose", done = false))
         }
     }
 }
