6df865143b33a3a974cdc02fd280299275c8fd53
Ioan Lazar <ilazar@Ioans-MacBook-Pro.local>
2025-10-31 09:23:06 +0200

    08

---
+++ app/src/main/java/com/example/myapp/todo/data/ItemRepository.kt
@@ -0,0 +1,21 @@
+package com.example.myapp.todo.data
+
+import android.util.Log
+
+object ItemRepository {
+    private val _items = List(100, { index ->
+        Item(id = "$index", text = "Item $index")
+    }).toMutableList()
+    val items: List<Item> = _items
+
+    fun update(id: String, text: String): Item? {
+        Log.d("ItemRepository", "update $id")
+        val index = _items.indexOfFirst { it -> it.id == id }
+        if (index != -1) {
+            val item = _items[index].copy(text = text)
+            _items.set(index, item)
+            return item
+        }
+        return null
+    }
+}
