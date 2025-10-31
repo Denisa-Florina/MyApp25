f69442339afc99d6d00d551a22906da477356457
Ioan Lazar <ilazar@Ioans-MacBook-Pro.local>
2025-10-31 10:26:08 +0200

    12

--- app/src/main/java/com/example/myapp/todo/ui/item/ItemViewModel.kt
+++ app/src/main/java/com/example/myapp/todo/ui/item/ItemViewModel.kt
@@ -6,30 +6,61 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myapp.MyApplication
+import com.example.myapp.core.Result
+import com.example.myapp.core.TAG
 import com.example.myapp.todo.data.Item
 import com.example.myapp.todo.data.ItemRepository
+import kotlinx.coroutines.launch
 
-data class ItemUiState(val item: Item?)
+data class ItemUiState(
+    val itemId: String? = null,
+    val item: Item,
+    var submitResult: Result<Item>? = null,
+)
 
-class ItemViewModelFactory(private val itemId: String?) : ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T = ItemViewModel(itemId) as T
-}
-
-class ItemViewModel(itemId: String?) : ViewModel() {
-    companion object {
-        val TAG = "ItemEditViewModel"
-    }
-
-    var uiState: ItemUiState by mutableStateOf(ItemUiState(ItemRepository.items.filter { it.id == itemId }[0]))
-        private set
-
+class ItemViewModel(private val itemId: String?, private val itemRepository: ItemRepository) :
+    ViewModel() {
     init {
         Log.d(TAG, "init")
     }
 
-    fun updateItem(id: String, text: String) {
-        Log.d(TAG, "addItem $text")
-        val item = ItemRepository.update(id, text)
-        uiState = ItemUiState(item)
+    var uiState: ItemUiState by mutableStateOf(
+        ItemUiState(item = itemRepository.getItem(itemId)?.copy() ?: Item())
+    )
+        private set
+
+    fun saveOrUpdateItem(text: String) {
+        viewModelScope.launch {
+            Log.d(TAG, "saveOrUpdateItem...");
+            try {
+                uiState = uiState.copy(submitResult = Result.Loading)
+                val item = uiState.item.copy(text = text)
+                val savedItem: Item;
+                if (itemId == null) {
+                    savedItem = itemRepository.save(item)
+                } else {
+                    savedItem = itemRepository.update(item)
+                }
+                Log.d(TAG, "saveOrUpdateItem succeeeded");
+                uiState = uiState.copy(submitResult = Result.Success(savedItem))
+            } catch (e: Exception) {
+                Log.d(TAG, "saveOrUpdateItem failed");
+                uiState = uiState.copy(submitResult = Result.Error(e))
+            }
+        }
+    }
+
+    companion object {
+        fun Factory(itemId: String?): ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val app =
+                    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MyApplication)
+                ItemViewModel(itemId, app.container.itemRepository)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myapp/todo/ui/items/ItemsScreen.kt
+++ app/src/main/java/com/example/myapp/todo/ui/items/ItemsScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -16,12 +17,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myapp.R
+import com.example.myapp.core.Result
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ItemsScreen(onItemClick: (id: String) -> Unit) {
-    Log.d("TodoScreen", "recompose")
-    val itemsViewModel = viewModel<ItemsViewModel>()
+fun ItemsScreen(onItemClick: (id: String?) -> Unit, onAddItem: () -> Unit) {
+    Log.d("ItemsScreen", "recompose")
+    val itemsViewModel = viewModel<ItemsViewModel>(factory = ItemsViewModel.Factory)
     val itemsUiState = itemsViewModel.uiState
     Scaffold(
         topBar = {
@@ -30,21 +32,30 @@ fun ItemsScreen(onItemClick: (id: String) -> Unit) {
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                    Log.d("ItemsScreen", "todo")
+                    Log.d("ItemsScreen", "add")
+                    onAddItem()
                 },
             ) { Icon(Icons.Rounded.Add, "Add") }
         }
     ) {
-        ItemList(
-            itemList = itemsUiState.items,
-            onItemClick = onItemClick,
-            modifier = Modifier.padding(it)
-        )
+        when (itemsUiState) {
+            is Result.Success ->
+                ItemList(
+                    itemList = itemsUiState.data,
+                    onItemClick = onItemClick,
+                    modifier = Modifier.padding(it)
+                )
+
+            is Result.Loading -> CircularProgressIndicator(modifier = Modifier.padding(it))
+            is Result.Error -> Text(
+                text = "Failed to load items - ${itemsUiState.exception?.message}",
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewItemsScreen() {
-    ItemsScreen(onItemClick = {})
+    ItemsScreen(onItemClick = {}, onAddItem = {})
 }
